7d050580d74ca72f3d23ea7769158ddacb4a8676
omer318 <62361067+omer318@users.noreply.github.com>
2023-11-05 02:50:01 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+
+def get_links():
+    """gets a link and folder name duo
+    like link|folder
+
+    Returns:
+        _type_: _description_
+    """
+    with open('queries.txt') as f:
+        return [row.split('|') for row in f.read().split()]
+
+def download(link,out_dir):
+    os.system(f'spotDL {link} --output={out_dir}')
+
+
+def main():
+    for link in get_links():
+        download(*link)
+
+
+if __name__ == "__main__":
+    main()
