77744c455ac3a8ec81f4660a89eb89a8cc5ae9e9
omer318 <62361067+omer318@users.noreply.github.com>
2023-11-08 19:16:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ def get_links():
         _type_: _description_
     """
     with open('queries.txt') as f:
-        return [row.split('|') for row in f.read().split()]
+        return [row.split('|') for row in f.read().split('\n') if "#" not in row]
 
 def download(link,out_dir):
-    os.system(f'spotDL {link} --output={out_dir}')
+    os.system(f'spotDL "{link}" --output="{out_dir}"')
 
 
 def main():
